624872f66519eddca6d275909220dfbb04f238a3
Mafube Mokone <126053664+Mafube@users.noreply.github.com>
2025-02-10 11:29:35 +0000

    using session to link pages

--- my_app/app_pages/list_events.py
+++ my_app/app_pages/list_events.py
@@ -1,7 +1,13 @@
 import streamlit as st
 
-
+# Button to navigate to the 'book.py' page
 if st.button("Booking"):
-    st.page_link("Go to Events", "app_pages/book.py")
+    st.session_state.page = "app_pages/book.py"  # Set a session state for navigation
+
+# Button to navigate to the 'create.py' page
 if st.button("Create Event"):
-    st.page_link("Go to Events", "app_pages/create.py")
+    st.session_state.page = "app_pages/create.py"  # Set a session state for navigation
+
+# Redirect to the selected page (if any)
+if "page" in st.session_state:
+    st.page_link("Go to Events", destination=st.session_state.page)
