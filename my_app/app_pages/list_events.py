b59f030b5bb9a97146730af6baab83fa9334295c
Mafube Mokone <126053664+Mafube@users.noreply.github.com>
2025-02-10 10:45:56 +0000

    creating new

--- my_app/app_pages/list_events.py
+++ my_app/app_pages/list_events.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 if st.button("Booking"):
     st.switch_page("app_pages/book.py")
-if st.button("List Events"):
-    st.switch_page("app_pages/list_events.py")
+if st.button("Create Event"):
+    st.switch_page("app_pages/create.py")
